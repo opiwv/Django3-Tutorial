22fd7a9f65f3935584c19218f4b82d38fa25f8d9
orion <orion.hollin@westpoint.edu>
2020-02-11 11:30:36 -0500

    migration, urls
    tutorial 2 from https://docs.djangoproject.com/en/3.0/intro/tutorial02/

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Greetings, Son of Mogh. Your house is honorable, and I pledge myself to your service!")
